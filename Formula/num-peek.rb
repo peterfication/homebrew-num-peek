f719119df1ad442b51707d990800edabb260b658
Peter Morgenstern <mail@petergundel.de>
2025-09-13 21:53:20 +0200

    Release version 0.1.3

--- Formula/num-peek.rb
+++ Formula/num-peek.rb
@@ -1,15 +1,15 @@
 class NumPeek < Formula
   desc ' A CLI tool to peek into *.npy files '
   homepage 'https://github.com/peterfication/num-peek'
-  version '0.1.2'
-  # https://github.com/peterfication/num-peek/releases/tag/0.1.2
+  version '0.1.3'
+  # https://github.com/peterfication/num-peek/releases/tag/0.1.3
 
   if Hardware::CPU.arm?
-    url 'https://github.com/peterfication/num-peek/releases/download/0.1.2/num-peek-v0.1.2-aarch64-apple-darwin.tar.gz'
-    sha256 '60249f995a24eef2bca0e49102ba56b89a4bdb44be7a295ce0fc34ea59d4955a'
+    url 'https://github.com/peterfication/num-peek/releases/download/0.1.3/num-peek-v0.1.3-aarch64-apple-darwin.tar.gz'
+    sha256 'e23a3afb50f1965fe0d47eb938526a30a654b90191c8fa7b73ffb782727c3e74'
   else
-    url 'https://github.com/peterfication/num-peek/releases/download/0.1.2/num-peek-v0.1.2-x86_64-apple-darwin.tar.gz'
-    sha256 '60ca808be8885441a965d2cfd254d336d238685e90d7fda8f7104f17f1cadddb'
+    url 'https://github.com/peterfication/num-peek/releases/download/0.1.3/num-peek-v0.1.3-x86_64-apple-darwin.tar.gz'
+    sha256 '8c04d9bd3e560d3d267ecb9cf51621effa0a51fbe1d343cefd226863d518e6d3'
   end
 
   def install
